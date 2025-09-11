dee46e9d66cb419ff09a73ab5eb09e254ab5fb04
username like :"Hamza-Maliikk <hamzamalik123450@gmail.com>
2025-09-11 20:06:04 +0500

    update commit

--- script.js
+++ script.js
@@ -11,17 +11,35 @@ async function getData(city) {
     let localData = new Date(utcSeconds * 1000);
     let date = localData.toLocaleDateString("en-US", { weekday: "long" });
     let time = localData.toLocaleTimeString("en-US");
+    console.log(time)
+    console.log(typeof(time))
+
     let result = document.getElementById("result");
+    let main = data.weather[0].main 
+    let description = data.weather[0].description
+    let icon = ""
+
+    if(main === "Clear"){
+      icon = "assets/images/clear/clear.png"
+    }else if(main === "Clouds"){
+        icon ="assets/images/clouds/cloud.png"
+    }else if(main === "Rain"){
+        icon ="assets/images/rain/heavy-rain.png"
+    }else if(main === "Thunderstorm"){
+        icon ="assets/images/Thunderstorm/thunder.png"
+    }
+
     result.innerHTML = `
       <h1 class="temp">${Math.floor(data.main.temp)}°C</h1>  
 
       <p>${date}</p>  
       <p>${time}</p>  
-      <p>${data.weather[0].main}</p>  
+      <p>${main}</p>  
+      <p>${description}</p>  
+      <img class="icon" src="${icon}" alt="">
       
       <p>Feels like ${data.main.feels_like}</p>  
       `;
-    console.log(data.weather[0].main);
   } catch (error) {
     console.log("Error ion weather", error);
   }
