af470c7fc9bc4d23f7f367583e5bc2b5904a8d82
username like :"Hamza-Maliikk <hamzamalik123450@gmail.com>
2025-09-16 00:53:00 +0500

    update commit

--- script.js
+++ script.js
@@ -5,43 +5,58 @@ async function getData(city) {
     let response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
+    console.log(response.status);
+
     let data = await response.json();
+
+    if (!response.ok) {
+      throw console.error(`Error in status ${response.status}`);
+    }
     console.log(data);
     let utcSeconds = data.dt + data.timezone;
     let localData = new Date(utcSeconds * 1000);
     let date = localData.toLocaleDateString("en-US", { weekday: "long" });
     let time = localData.toLocaleTimeString("en-US");
-    console.log(time)
-    console.log(typeof(time))
+    console.log(time);
+    console.log(typeof time);
 
+    let name = data.name;
     let result = document.getElementById("result");
-    let main = data.weather[0].main 
-    let description = data.weather[0].description
-    let icon = ""
+    let main = data.weather[0].main;
+    let description = data.weather[0].description;
+    let bg = ""
+    let icon = "";
 
-    if(main === "Clear"){
-      icon = "assets/images/clear/clear.png"
-    }else if(main === "Clouds"){
-        icon ="assets/images/clouds/cloud.png"
-    }else if(main === "Rain"){
-        icon ="assets/images/rain/heavy-rain.png"
-    }else if(main === "Thunderstorm"){
-        icon ="assets/images/Thunderstorm/thunder.png"
+    if (main === "Clear") {
+      icon = "assets/images/clear/clear.png";
+      bg = "./assets/images/clear/clear_bg.png"
+    } else if (main === "Clouds") {
+      icon = "assets/images/clouds/cloud.png";
+    } else if (main === "Rain") {
+      icon = "assets/images/rain/heavy-rain.png";
+    } else if (main === "Thunderstorm") {
+      icon = "assets/images/Thunderstorm/thunder.png";
     }
 
     result.innerHTML = `
+    <div class="icons">
+      <img src=${icon} alt="">
+      </div>
+    <div class="temperature">
+
       <h1 class="temp">${Math.floor(data.main.temp)}°C</h1>  
 
       <p>${date}</p>  
       <p>${time}</p>  
       <p>${main}</p>  
       <p>${description}</p>  
-      <img class="icon" src="${icon}" alt="">
-      
       <p>Feels like ${data.main.feels_like}</p>  
+      <p>${name}</p> 
+      </div>
+   
       `;
   } catch (error) {
-    console.log("Error ion weather", error);
+    console.log("Error in weather", error);
   }
 }
 function submitButton() {
